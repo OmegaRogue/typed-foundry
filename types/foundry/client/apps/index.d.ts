93f1036cedfb99b1b2662028b57ecfab90c2f758
OmegaRogue <omegarogue@omegavoid.codes>
2024-02-09 22:45:38 +0100

    :label: Type updates

--- types/foundry/client/apps/index.d.ts
+++ types/foundry/client/apps/index.d.ts
@@ -1,6 +1,6 @@
 import "./app.d.ts";
 import "./form.d.ts";
-import "./forms/index.d.ts";
+import "./forms";
 import "./i18n.d.ts";
-import "./sidebar/index.d.ts";
+import "./sidebar";
 import "./templates.d.ts";
